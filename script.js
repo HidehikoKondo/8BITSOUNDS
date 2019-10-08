a11659e6fcebe1895ffa0eabab8dfd65310f2cf5
Hidehiko Kondo <dev@udonko.net>
2019-10-08 17:00:19 +0900

    レイアウト調整などなど

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-function openMenu(name) {
+function openMenu(name, title) {
     $("#contents").html("");
+    $("#top").html("");
+    $("#headline").html(title);
+
 
     for (i = 1; i <= 100; i++) {
         console.log(name);
-
-        $("#contents").append('<div class="player"><audio src="sounds/mp3/' + name + i + '.mp3" preload controls></audio></div> <div class="dl"><a href="../../image/design/tagindex_mini.gif" download="sounds/mp3/Blip_Select10.mp3">' + name + i + 'mp3 ダウンロード</a></div>');
-
-
+        $("#contents").append('<div class="player"><audio src="sounds/mp3/' + name + i + '.mp3" preload download controls></audio></div> <div class="dl"><a href="sounds/mp3/' + name + i + '.mp3" download="' + title + i + '"><i class="fas fa-download"></i> ' + title + i + '.mp3</a></div>');
     }
 }
