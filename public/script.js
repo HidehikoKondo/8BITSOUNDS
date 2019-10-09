e0b35b42f10807d0f6c7569097697920396025bf
UDONKONET <dev@udonko.net>
2019-10-09 23:11:27 +0900

    とりあえずリリースバージョン１

--- public/script.js
+++ public/script.js
@@ -1,11 +1,13 @@
-function openMenu(name, title) {
-    $("#contents").html("");
-    $("#top").html("");
-    $("#headline").html(title);
+$(function () {
+    $("#download").hide();
+});
 
+function openMenu(name, title) {
+    $("#about").hide();
+    $("#download").show();
+    $("#download").html("");
 
     for (i = 1; i <= 100; i++) {
-        console.log(name);
-        $("#contents").append('<div class="player"><audio src="sounds/mp3/' + name + i + '.mp3" preload download controls></audio></div> <div class="dl"><a href="sounds/mp3/' + name + i + '.mp3" download="' + title + i + '"><i class="fas fa-download"></i> ' + title + i + '.mp3</a></div>');
+        $("#download").append('<div class="player"><audio src="sounds/mp3/' + name + i + '.mp3" preload download controls></audio></div> <div><a href="sounds/mp3/' + name + i + '.mp3" download="' + title + i + '"><i class="fas fa-download"></i> ' + title + i + '.mp3</a></div>');
     }
 }
